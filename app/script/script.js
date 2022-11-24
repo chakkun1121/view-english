b8eda83b8482b85b17571d4ac4f509a75a2f41d2
chakkun <99847806+chakkun1121@users.noreply.github.com>
2022-11-24 01:30:39 +0000

    基礎ライブラリをjsから読み込めるように

--- app/script/script.js
+++ app/script/script.js
@@ -1,9 +1,17 @@
 const appEvent = document.getElementById('appEvent');
 window.onload = function () {
   //表示の準備をする
+  for (let i = 0; i < librariesURL.length; i++) {
+    const script = document.createElement('script');
+    script.setAttribute('src', librariesURL[i]);
+    document.head.appendChild(script);
+  }
   //タブ表示の拡張機能の準備
   appEvent.dispatchEvent(new Event('setTab'))
 
   //いつでも使用可能になったらローミング画面を消す
   document.getElementById('loading').classList.add('loaded');
 }
+const librariesURL = [
+  "script/tab.js"
+]
