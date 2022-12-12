d8c60bf9c23f5f8a1750a59b9fe89aaf91592e79
chakkun <99847806+chakkun1121@users.noreply.github.com>
2022-12-12 12:06:11 +0900

    バージョンアップ準備

--- app/script/script.js
+++ app/script/script.js
@@ -1,5 +1,5 @@
 const appEvent = document.getElementById('appEvent');
-const appVersion = "4.0."
+const appVersion = "4.0.5"
 window.onload = function () {
   //表示の準備をする
   const setStart = setInterval(function () {
