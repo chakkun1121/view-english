6b1afa23b83648cfb7083527e473ed2e3ec3999e
chakkun1121 <chakkun1121haruki@gmail.com>
2022-12-26 18:26:50 +0900

    設定機能の追加(不調)

--- app/settings/script.js
+++ app/settings/script.js
@@ -1,11 +1,33 @@
 const app = document.querySelector('main');
 function Main() {
-  return <h2>設定一覧</h2>
-}
-function settingsHeader() {
   return <>
-    <h2>設定一覧</h2>
-    <p>下から変更したい設定を指定してください</p>
+    <Settingsheader />
+    <Rendsettings />
   </>
 }
-ReactDOM.render(<Main/>, app)
+function Settings() {
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", function () {
+    console.log(this.responseText);
+    Rendsettings(<h3>準備完了</h3>)
+  });
+  req.addEventListener("error", function () {
+    Rendsettings(<>
+      <h3 className="error">インターネット接続に失敗しました。</h3>
+      <p>エラーコード:{error}</p>
+    </>)
+  });
+  req.open("GET", "settings.json")//なぜか動かない
+  req.send();
+  Rendsettings(<></>)
+}
+function Rendsettings({ settings }) {
+  return <>{settings}</>
+}
+function Settingsheader() {
+  return <div className="settings-header">
+    <h2>設定一覧</h2>
+    <p>下から変更したい設定を指定してください</p>
+  </div>
+}
+ReactDOM.render(<Main />, app)
