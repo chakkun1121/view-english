17fcde6b66b957754b6df62c9d4f1ed3c84ff67c
chakkun1121 <chakkun1121haruki@gmail.com>
2022-12-26 17:53:15 +0900

    設定関連を追加

---
+++ app/settings/script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  
+}
+function Viewsettings() {
+  return <p>読み込み中</p> 
+}
+const settings = document.getElementById('settings');
+ReactDOM.render(<Viewsettings/>, settings);
