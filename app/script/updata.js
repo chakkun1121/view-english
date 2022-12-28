63df8cd4a481f1337cfcac2d8dd7c378c63f0fe6
chakkun1121 <chakkun1121haruki@gmail.com>
2022-12-28 18:36:44 +0900

    手動更新ボタン作成

--- app/script/updata.js
+++ app/script/updata.js
@@ -1,11 +1,16 @@
+document.getElementById('headerFileMenu').innerHTML += `
+  <button class="header-file-menu-button" onclick="updata()">アプリの手動更新</button>
+`
 //アップデート用
 function updata() {
-  navigator.serviceWorker.getRegistrations().then(function (registrations) {
-    // 登録されているworkerを全て削除する
-    for (let registration of registrations) {
-      registration.unregister();
-    }
-  });
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      // 登録されているworkerを全て削除する
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    });
+  }
   caches.keys().then(function (keys) {
     var promises = [];
     // キャッシュストレージを全て削除する
