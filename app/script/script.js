8d092feef7f7cf490b2dd813d1570a5d827e5b44
chakkun <99847806+chakkun1121@users.noreply.github.com>
2023-01-05 17:20:14 +0900

    動かないバグがあったので訂正

--- app/script/script.js
+++ app/script/script.js
@@ -1,7 +1,7 @@
 const appEvent = document.getElementById('appEvent');
 const appVersion = "4.0.7"
 window.onload = function () {
-  const useLibraryURLs = librariesURL.concat(JSON.parse(localStorage.getItem('wayakuExtensions')))
+  const useLibraryURLs = JSON.parse(localStorage.getItem('wayakuExtensions').length?librariesURL.concat(JSON.parse(localStorage.getItem('wayakuExtensions'))):libraryURL
   console.log(useLibraryURLs)
   //表示の準備をする
   const setStart = setInterval(function () {
