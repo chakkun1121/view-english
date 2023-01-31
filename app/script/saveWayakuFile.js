b56fd0b9c52d3d05e99dfadff1cb967a2a3d6a0c
chakkun1121 <chakkun1121haruki@gmail.com>
2023-02-01 08:32:05 +0900

    保存時にfilehandingapiを利用

--- app/script/saveWayakuFile.js
+++ app/script/saveWayakuFile.js
@@ -1,22 +1,35 @@
 function saveWayaku(tabID = tab.openedTab()) {
-  const wayakuArrary = viewHTMLtoArray(tab.HTMLcontent.get(tabID))
-  const title = arrayToText(viewHTMLtoArray(tab.HTMLcontent.get(tabID)))[0] + ".wayaku"
-  const wayakuData = arrayToWayaku(wayakuArrary)
-  downloadWayaku(title, wayakuData)
+  const wayakuArrary = viewHTMLtoArray(tab.HTMLcontent.get(tabID));
+  const title = arrayToText(viewHTMLtoArray(tab.HTMLcontent.get(tabID)))[0] + '.wayaku';
+  const wayakuData = arrayToWayaku(wayakuArrary);
+  downloadWayaku(title, wayakuData);
 }
-function downloadWayaku(fileName, data) {
-  const blob = new Blob([data], { type: 'application/dxf' });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  document.body.appendChild(a);
-  a.download = fileName;
-  a.href = url;
-  a.click();
-  a.remove();
-  URL.revokeObjectURL(url);
+async function saveWayakuFile(fileHandle, contents) {
+  console.log(contents);
+  // writable作成
+  const writable = await fileHandle.createWritable();
+  // コンテンツを書き込む
+  await writable.write(contents);
+
+  // ファイル閉じる
+  await writable.close();
+}
+async function downloadWayaku(fileName, data) {
+  const handle = await window.showSaveFilePicker({
+    types: [
+      {
+        description: 'wayakuファイル',
+        accept: {
+          'text/wayaku': ['.wayaku'],
+        },
+      },
+    ],
+    suggestedName: fileName,
+  });
+  await saveWayakuFile(handle, data);
 }
 
 document.getElementById('headerFileMenu').innerHTML += `
   <button class="header-file-menu-button" onclick="saveWayaku()">保存</button>
-`
-finishedScriptNumber++
+`;
+finishedScriptNumber++;
