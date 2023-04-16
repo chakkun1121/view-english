926fff7086d801c83bd5034ccae4a95e77708374
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-04-16 23:48:15 +0000

    next.jsでファイルが開けるようになった。

---
+++ src/pages/app/index.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+async function loadFile() {
+  // "Open File" ダイアログを開く
+  if (!window.showOpenFilePicker) {
+    console.error('showOpenFilePickerが利用できません。和訳表示サイト軽量版をご利用ください。');
+    return;
+  }
+  const fhList = await window.showOpenFilePicker({
+    types: [
+      {
+        description: 'wayakuファイル',
+        accept: {
+          'application/dxf': ['.wayaku'],
+        },
+      },
+      {
+        description:
+          '和訳HTMLファイル(配布終了ずみだが互換性を保つためにあります。そろそろ廃止しますので和訳ファイルに変換してください。)',
+        accept: {
+          'text/html': ['.html'],
+        },
+      },
+    ],
+    excludeAcceptAllOption: true,
+    multiple: true,
+    mode: 'readwrite',
+  });
+  const file = await fhList[0].getFile();
+  return file.text();
+}
+
+function WayakuApp() {
+  const [wayakuData, setWayakuData] = useState('');
+
+  const handleFileChange = async () => {
+    const fileText = await loadFile();
+    setWayakuData(fileText);
+  };
+
+  return (
+    <div>
+      <button onClick={handleFileChange}>ファイルを選択する</button>
+      <div>{wayakuData}</div>
+    </div>
+  );
+}
+
+export default WayakuApp;
