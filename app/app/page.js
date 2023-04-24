1de856ca0f0a8aa8625e37c5466b3a78e03f7f33
chakkun <chakkun1121haruki@gmail.com>
2023-04-25 08:42:33 +0900

    タブID追加(またエラー発生)

--- app/app/page.js
+++ app/app/page.js
@@ -1,13 +1,10 @@
 'use client';
-import { getQuery } from '@/library/getQuery';
 import MainContent from './mainContent';
-export const mainColor="#7aa1d6";
+export const mainColor = '#7aa1d6';
 export default function App() {
-  const query = getQuery();
-  console.log(query);
   return (
     <>
-      <MainContent fileID={query?.fileID || null} type={query?.type || 'newTab'} />
+      <MainContent />
     </>
   );
 }
--- app/app/tabs.js
+++ app/app/tabs.js
@@ -1,13 +1,21 @@
 'use client';
+import uuid from 'uuid';
 import { useState } from 'react';
 import { Tab, Tabs } from '@mui/material';
 export default function AppTabs() {
-  const [tabs, setTabs] = useState([{ title: '新しいタブ', type: 'newTab' }]);
+  console.log(typeof `tab-${uuid.v4()}`);
+  const [tabs, setTabs] = useState([
+    { title: '新しいタブ', type: 'newTab', tabID: `tab-${uuid.v4()}` },
+  ]);
+  function newTab() {
+    setTabs([...tabs, { title: '新しいタブ', type: 'newTab', tabID: `tab-${uuid.v4()}` }]);
+  }
   return (
     <Tabs sx={{ background: '#cccccc', padding: 0 }} variant="scrollable" scrollButtons="auto">
       {tabs.map((tab) => {
         return <Tab label={tab.title} />;
       })}
+      <button onClick={newTab}>+</button>
     </Tabs>
   );
 }
