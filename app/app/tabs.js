8581b038383592de2f20f89203abd97326029b9f
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-04-25 04:11:27 +0000

    バグ修正

--- app/app/tabs.js
+++ app/app/tabs.js
@@ -1,13 +1,13 @@
 'use client';
-import uuid from 'uuid';
+import { v4 as getUUID } from 'uuid';
 import { useState } from 'react';
 import { Tab, Tabs } from '@mui/material';
 export default function AppTabs() {
   const [tabs, setTabs] = useState([
-    { title: '新しいタブ', type: 'newTab', tabID: `tab-${uuid.v4()}` },
+    { title: '新しいタブ', type: 'newTab', tabID: `tab-${getUUID()}` },
   ]);
   function newTab() {
-    setTabs([...tabs, { title: '新しいタブ', type: 'newTab', tabID: `tab-${uuid.v4()}` }]);
+    setTabs([...tabs, { title: '新しいタブ', type: 'newTab', tabID: `tab-${getUUID()}` }]);
   }
   return (
     <Tabs sx={{ background: '#cccccc', padding: 0 }} variant="scrollable" scrollButtons="auto">
