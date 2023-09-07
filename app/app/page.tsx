386bccfe0afb4672e18304d22e2598c8cfd81cb2
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-07 00:30:55 +0000

    ファイルを開けるようにした。

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,21 +1,45 @@
 'use client';
 import Link from 'next/link';
 import { JSX, ClassAttributes, ButtonHTMLAttributes, useState } from 'react';
+import { openWayakuFile } from './lib/wayaku';
+import { wayakuObject } from '../../@types/wayakuObjectType';
 
 export default function app() {
-  const [fileContent, setFileContent] = useState<string>('');
+  const [fileContent, setFileContent] = useState<wayakuObject>();
   return (
     <>
       <header className="print-hidden">
         <nav className="flex bg-main">
-          <NabButton title="ファイルを開く">開く</NabButton>
+          <NabButton
+            title="ファイルを開く"
+            onClick={async () => {
+              const file = await openWayakuFile();
+              setFileContent(file);
+            }}
+          >
+            開く
+          </NabButton>
           <NabButton title="英文、日本語訳の色を変更します">表示変更</NabButton>
           <NabButton>保存</NabButton>
           <NabButton title="フラッシュカードをスタート">フラッシュカード</NabButton>
           <NabButton>設定</NabButton>
         </nav>
       </header>
-      <main></main>
+      <main>
+        {fileContent ? (
+          <div className="p-4">
+            <h1 className="text-2xl font-bold">{fileContent.wayaku.h1['#text']}</h1>
+            {fileContent.wayaku.section.map((section) => (
+              <section key={section['@_id']}>
+                <p lang="en">{section.p[0]['#text']}</p>
+                <p lang="ja">{section.p[1]['#text']}</p>
+              </section>
+            ))}
+          </div>
+        ) : (
+          <> </>
+        )}
+      </main>
       <div className="absolute bottom-10 right-10 print:hidden">
         <div className="p-2 m-2 rounded-full bg-gray-100 hover:bg-gray-200 w-20 h-20 justify-center flex items-center">
           <Link href="../help" className="text-black hover:text-center visited:text-center">
