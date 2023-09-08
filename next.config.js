b292c70eb667fe059af3dfff55ae54e62ce54f44
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-08 15:57:06 +0900

    ビルド準備

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  basePath: process.env.NODE_ENV === 'development' ? '' : 'view-english',
+  basePath: process.env.NODE_ENV === 'development' ? '' : '/view-english',
+  output: 'export',
 };
 
 module.exports = nextConfig;
