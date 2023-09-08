d7219668b72ad357d3e03f997c7c3d889ebd22f6
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-08 15:54:04 +0900

    一応公開できるところまで持ってきた

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,12 +4,23 @@ import React from 'react';
 export default function Home() {
   return (
     <>
-      <section>
-        <img src="./img/view-english.webp" alt="和訳表示サイト" />
+      <section className="grid justify-center">
+        <Link href="./view-english.pdf" target="_blank">
+          <img src="./img/view-english.webp" alt="和訳表示サイト" />
+        </Link>
       </section>
       <section className="p-2">
         <h2>アプリへのリンク</h2>
-        <Link href="./app">通常版</Link>
+        <ul>
+          <li>
+            <Link href="./app">通常版(対応ブラウザ:PC版GoogleChrome最新版)</Link>
+          </li>
+          <li>
+            <a href="./lightVersion/index.html">
+              軽量版(通常版が動かない端末ではこちらをご利用ください。)
+            </a>
+          </li>
+        </ul>
       </section>
     </>
   );
