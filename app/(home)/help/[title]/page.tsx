4d2c64ce02b34aab47636a48fa2eff22d3823357
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-08 18:02:36 +0900

    ヘルプページの作成

---
+++ app/(home)/help/[title]/page.tsx
@@ -0,0 +1,48 @@
+import path from "path";
+import { getHelpPageTitles } from "../lib/getHelpPageTitles";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { getHelpFileData } from "../lib/getHelpFileData";
+import { Metadata } from "next";
+import Link from "next/link";
+export default async function IndividualHelpPage({
+  params,
+}: {
+  params: { title: string };
+}) {
+  const { title } = params;
+  const filePath = path.join(process.cwd(), "public", "help", `${title}.md`);
+  const data = await getHelpFileData(filePath);
+  // mdのheader部分を除去したファイルを準備する
+  const renderFile: string = data.file.replace(/^---[\s\S]*?---/, "");
+  return (
+    <>
+      <Link href="./">ヘルプ記事一覧へ戻る</Link>
+      <article>
+        <h1>{data.title}</h1>
+        <ReactMarkdown>{renderFile}</ReactMarkdown>
+      </article>
+    </>
+  );
+}
+export async function generateMetadata({
+  params,
+}: {
+  params: { title: string };
+}): Promise<Metadata> {
+  const { title } = params;
+  const filePath = path.join(process.cwd(), "public", "help", `${title}.md`);
+  const data = await getHelpFileData(filePath);
+  return {
+    title: data.title,
+    description: data.description,
+    alternates: {
+      canonical: `/help/${title}`,
+    },
+  };
+}
+export async function generateStaticParams(): Promise<{ title: string }[]> {
+  const paths = await getHelpPageTitles();
+  return paths.map((title) => ({
+    title,
+  }));
+}
---
+++ app/(home)/help/page.tsx
@@ -0,0 +1,40 @@
+import path from 'path';
+import { getHelpFileData } from './lib/getHelpFileData';
+import { getHelpPageTitles } from './lib/getHelpPageTitles';
+import Link from 'next/link';
+import { helpFileDataType } from '../../../@types/helpFileDataType';
+
+export default async function Help() {
+  const helpPageTitles: string[] = await getHelpPageTitles();
+  console.debug(helpPageTitles);
+  const helpPagesData: helpFileDataType[] = await Promise.all(
+    helpPageTitles.map(async (title) => {
+      const filePath = path.join(process.cwd(), 'public', 'help', `${title}.md`);
+      const data = await getHelpFileData(filePath);
+      return data;
+    })
+  );
+  return (
+    <>
+      <section>
+        <h1>ヘルプ記事一覧</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {helpPagesData.map((data, i) => (
+            <Link
+              key={i}
+              className="bg-main rounded hover:bg-main-hover p-4 text-black hover:text-black visited:text-black"
+              href={`help/${helpPageTitles[i]}`}
+            >
+              <h2 className="line-clamp-2" title={data.title}>
+                {data.title}
+              </h2>
+              <p className="line-clamp-2" title={data.description}>
+                {data.description}
+              </p>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
