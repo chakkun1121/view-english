f9bbed3052b9b5194ed85bd47b9bc50ead7e9b13
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-08 18:42:31 +0900

    パスの修正

--- app/app/_components/fav.tsx
+++ app/app/_components/fav.tsx
@@ -6,7 +6,7 @@ export function Fav() {
   return (
     <div className="fixed bottom-10 right-10 print:hidden select-none w-16 h-16 rounded-full bg-gray-100 hover:bg-gray-200 z-50">
       <Link
-        href="../help"
+        href="./help"
         target="_blank"
         className="text-black text-center hover:text-black visited:text-black  w-full h-full"
       >
