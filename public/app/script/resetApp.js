e95cad44262b478b99bfb52bf94dba02150a7480
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-08 15:58:09 +0900

    ファイル整理

--- public/app/script/resetApp.js
+++
@@ -1,7 +0,0 @@
-function resetApp() {
-  localStorage.removeItem('tabInfo');
-  localStorage.removeItem('cards');
-  localStorage.removeItem('wayakuSettings');
-  localStorage.removeItem('wayakuExtensions');
-  updata();
-}
