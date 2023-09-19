29beb98d972b44c967138027ff4027153c4dd7ba
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-19 17:43:32 +0900

    これでIE以外のブラウザはなんとかした

--- app/app/lib/openWayakuFile.ts
+++ app/app/lib/openWayakuFile.ts
@@ -8,34 +8,54 @@ export async function openWayakuFile(): Promise<{
 }> {
   return new Promise<{ wayakuObject: wayakuObject; fileHandle: FileSystemFileHandle }>(
     async (resolve, reject) => {
-      if (!window.showOpenFilePicker) reject('このブラウザではファイルを開けません。');
-      const [fileHandle]: [FileSystemFileHandle] = await window.showOpenFilePicker({
-        types: [
-          {
-            description: 'wayakuファイル',
-            accept: {
-              'application/dxf': ['.wayaku'],
+      if (window.showOpenFilePicker) {
+        const [fileHandle]: [FileSystemFileHandle] = await window.showOpenFilePicker({
+          types: [
+            {
+              description: 'wayakuファイル',
+              accept: {
+                'application/dxf': ['.wayaku'],
+              },
             },
-          },
-          {
-            description: '和訳HTMLファイル(配布終了ずみだが互換性を保つためにあります。)',
-            accept: {
-              'text/html': ['.html'],
+            {
+              description: '和訳HTMLファイル(配布終了ずみだが互換性を保つためにあります。)',
+              accept: {
+                'text/html': ['.html'],
+              },
             },
-          },
-        ],
-        excludeAcceptAllOption: true,
-      });
-      let file = await fileHandle.getFile();
-      let reader = new FileReader();
-      reader.readAsText(file);
-      reader.onload = () => {
-        resolve({
-          wayakuObject: stringToObject(fixWayakuFile(reader.result as string)),
-          fileHandle,
+          ],
+          excludeAcceptAllOption: true,
         });
-      };
-      reader.onerror = reject;
+        let file = await fileHandle.getFile();
+        let reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => {
+          resolve({
+            wayakuObject: stringToObject(fixWayakuFile(reader.result as string)),
+            fileHandle,
+          });
+        };
+        reader.onerror = reject;
+      } else {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = '.wayaku';
+        input.onchange = () => {
+          if (input.files) {
+            if (!input.files[0].name.endsWith('.wayaku')) reject('wayakuファイルではありません。');
+            let reader = new FileReader();
+            reader.readAsText(input.files[0]);
+            reader.onload = () => {
+              resolve({
+                wayakuObject: stringToObject(fixWayakuFile(reader.result as string)),
+                fileHandle: undefined,
+              });
+            };
+            reader.onerror = reject;
+          }
+        };
+        input.click();
+      }
     }
   );
 }
