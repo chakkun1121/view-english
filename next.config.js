683f604b503279431c3dbab6f44997ab35bc59e3
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-09-20 17:13:03 +0900

    Revert "Feature/#119"

--- next.config.js
+++ next.config.js
@@ -6,19 +6,6 @@ const nextConfig = withPWA({
   /* config options here */
   basePath: process.env.NODE_ENV === 'development' ? '' : '/view-english',
   output: 'export',
-  runtimeCaching: [
-    // すべてのファイルをキャッシュし、オフライン対応させる
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
 });
 
 module.exports = nextConfig;
