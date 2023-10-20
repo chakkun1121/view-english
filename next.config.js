e11bf889339cb2843b40ac2ec5e85057b2a9224f
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-10-20 05:12:26 +0000

    change

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
 });
+const SUB_DIRECTORY = process.env.SUB_DIRECTORY || '';
+const isProd = process.env.NODE_ENV == 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
   /* config options here */
-  basePath: process.env.NODE_ENV === 'development' ? '' : '/view-english',
+  basePath: isProd ? SUB_DIRECTORY : '',
   output: 'export',
+  assetPrefix: isProd ? SUB_DIRECTORY : '',
+  publicRuntimeConfig: {
+    basePath: isProd ? SUB_DIRECTORY : '',
+  },
   runtimeCaching: [
     // すべてのファイルをキャッシュし、オフライン対応させる
     {
