257945f6d851b82a10f4b3e6fc179f19d9647eed
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-10-31 03:45:49 +0000

    設定を保存できるようにした。

--- app/app/lib/settings.ts
+++ app/app/lib/settings.ts
@@ -1,13 +1,20 @@
 import { atom, useRecoilState } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
 export interface settingsType {
   isDarkMode: boolean;
 }
+const { persistAtom } = recoilPersist({
+  //追加
+  key: 'settings',
+  storage: typeof window === 'undefined' ? undefined : localStorage,
+});
 export const settingsAtom = atom<settingsType>({
   key: 'settings',
   default: {
     isDarkMode: false,
   },
+  effects_UNSTABLE: [persistAtom],
 });
 export const settingsMenu = [
   {
