8f8bfbd5c738908086916b0dcc1d27409a6e3407
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-10-31 02:10:04 +0000

    設定を追加

--- app/app/_components/settingsPage.tsx
+++ app/app/_components/settingsPage.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useRecoilState } from 'recoil';
-import { settingsAtom } from '../lib/settings';
+import { settingsAtom, settingsMenu } from '../lib/settings';
 import { CloseButton } from './CloseButton';
 import { useHotkeys } from 'react-hotkeys-hook';
 
@@ -10,10 +10,26 @@ export default function SettingsPage({ close }: { close: () => void }) {
   });
   const [settings, setSettings] = useRecoilState(settingsAtom);
   return (
-    <section>
-      <div className="flex">
+    <section className="select-none">
+      <div className="flex p-2">
         <h2 className="flex-1">設定</h2>
         <CloseButton close={close} />
+      </div>
+      <div className="flex flex-col gap-4 p-4">
+        {settingsMenu.map((setting) => (
+          <label className="p-2 rounded bg-gray-100 flex items-center">
+            <input
+              type="checkbox"
+              checked={settings[setting.key]}
+              onChange={(e) => setSettings({ ...settings, [setting.key]: e.target.checked })}
+              className="flex-none"
+            />
+            <span className="flex-1">
+              {setting.title}
+              {setting.isFlag && ' (flag)'}
+            </span>
+          </label>
+        ))}
       </div>
     </section>
   );
--- app/app/lib/settings.ts
+++ app/app/lib/settings.ts
@@ -1,8 +1,19 @@
 import { atom, useRecoilState } from 'recoil';
 
-export interface settingsType {}
+export interface settingsType {
+  isDarkMode: boolean;
+}
 export const settingsAtom = atom<settingsType>({
   key: 'settings',
-  default: {},
+  default: {
+    isDarkMode: false,
+  },
 });
-
+export const settingsMenu = [
+  {
+    key: 'isDarkMode',
+    type: 'boolean',
+    title: 'ダークモード',
+    isFlag: true,
+  },
+];
