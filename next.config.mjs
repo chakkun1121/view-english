c83b5fb366ce0310a74facaeef89aa88a4d25e9a
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-11-02 04:27:22 +0000

    remark-gfmインストール

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,26 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-});
+import nextPWA from 'next-pwa';
+import nextMDX from '@next/mdx';
+import remarkGfm from 'remark-gfm';
+
 const SUB_DIRECTORY = process.env.SUB_DIRECTORY || '';
 const isProd = process.env.NODE_ENV == 'production';
-const withMDX = require('@next/mdx')({
+const withPWA = nextPWA({
+  dest: 'public',
+});
+const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
     // If you use remark-gfm, you'll need to use next.config.mjs
     // as the package is ESM only
     // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
+    remarkPlugins: [remarkGfm],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
 });
 /** @type {import('next').NextConfig} */
-const nextConfig = withMDX(
+export default withMDX(
   withPWA({
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     /* config options here */
@@ -28,5 +32,3 @@ const nextConfig = withMDX(
     },
   })
 );
-
-module.exports = nextConfig;
