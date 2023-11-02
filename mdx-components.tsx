4aeb2e4fc2c30ab10875b8a8fb99dbc49ec7686e
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-11-02 04:51:05 +0000

    スタイルをmdx側で設定

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -10,6 +10,13 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     // Allows customizing built-in components, e.g. to add styling.
     // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
+    h1: ({ children }) => <h1 className="pl-0">{children}</h1>,
+    h2: ({ children }) => <h2 className="pl-1">{children}</h2>,
+    h3: ({ children }) => <h3 className="pl-2">{children}</h3>,
+    h4: ({ children }) => <h4 className="pl-3">{children}</h4>,
+    h5: ({ children }) => <h5 className="pl-4">{children}</h5>,
+    h6: ({ children }) => <h6 className="pl-5">{children}</h6>,
+    p: ({ children }) => <p className="pl-6">{children}</p>,
     ...components,
   };
 }
