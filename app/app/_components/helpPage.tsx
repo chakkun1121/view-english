770c9f31e52b6f906e175fdd4954cf7e5d818557
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-11-09 04:19:56 +0000

    winboxを入れたことで目標に近づいた
    ヘッダー非表示機能をこの後作る

--- app/app/_components/helpPage.tsx
+++ app/app/_components/helpPage.tsx
@@ -1,66 +1,20 @@
 'use client';
-import { useEffect, useState } from 'react';
-import { CloseButton } from './CloseButton';
-import { getAllHelpPageMetaApiType } from '../../api/getAllHelpPageMeta/route';
-import path from 'path';
+import WinBox from '../../../winbox';
 
 export function HelpPage({
   setIsShowHelpPage,
 }: {
   setIsShowHelpPage: (isShowHelpPage: boolean) => void;
 }) {
-  const [currentPath, setCurrentPath] = useState<string>('');
-  const [allHelpPageData, setAllHelpPageData] = useState<getAllHelpPageMetaApiType[] | undefined>();
-  useEffect(() => {
-    async function fetchData() {
-      const result = await fetch('api/getAllHelpPageMeta');
-      setAllHelpPageData(await result.json());
-    }
-    fetchData();
-  }, []);
   return (
-    <div className="absolute top-2 md:top-16 md:bottom-0 right-4 left-4 bottom-16 bg-primary border z-50 rounded p-2">
-      <div className="flex">
-        <h2 className="flex-1">ヘルプ</h2>
-        <CloseButton close={() => setIsShowHelpPage(false)} />
-      </div>
-      <div>
-        {currentPath === '' ? (
-          <>
-            {allHelpPageData ? (
-              <ul>
-                {allHelpPageData.map((data) => (
-                  <li key={data.path}>
-                    <a
-                      onClick={() => {
-                        setCurrentPath(data.path);
-                      }}
-                    >
-                      {data.title}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>読み込み中</p>
-            )}
-          </>
-        ) : (
-          <Help path={currentPath} />
-        )}
-      </div>
-    </div>
+    <WinBox
+      title="ヘルプ"
+      noFull={true}
+      y={60}
+      z={100}
+      url="help?header=false"
+      themeColor="#7aa1d6"
+      onClose={() => setIsShowHelpPage(false)}
+    />
   );
 }
-function Help({ path }: { path: string }) {
-  const [content, setContent] = useState<string | undefined>();
-  useEffect(() => {
-    async function fetchData() {
-      const result = await fetch(`api/getHelpPage/${path}`);
-      setContent(await result.text());
-    }
-    fetchData();
-  }, [path]);
-  return <>{content}</>;
-}
-
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import nextPWA from 'next-pwa';
 import nextMDX from '@next/mdx';
 import remarkGfm from 'remark-gfm';
-
+import nextTranspileModules from 'next-transpile-modules';
 const SUB_DIRECTORY = process.env.SUB_DIRECTORY || '';
 const isProd = process.env.NODE_ENV == 'production';
 const withPWA = nextPWA({
@@ -19,9 +19,10 @@ const withMDX = nextMDX({
     // providerImportSource: "@mdx-js/react",
   },
 });
+const withTM = nextTranspileModules(['react-winbox']);
 /** @type {import('next').NextConfig} */
 export default withMDX(
-  withPWA({
+  withPWA(withTM())({
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     /* config options here */
     basePath: isProd ? SUB_DIRECTORY : '',
