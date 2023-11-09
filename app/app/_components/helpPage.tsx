4c015c86944bd8bb6afca364ea7d1f2600d7be85
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-11-09 06:12:37 +0000

    記事を目標の場所に置けた

--- app/app/_components/helpPage.tsx
+++ app/app/_components/helpPage.tsx
@@ -10,10 +10,16 @@ export function HelpPage({
     <WinBox
       title="ヘルプ"
       noFull={true}
+      top={60}
+      bottom={60}
+      x={document.body.clientWidth - (Math.min(document.body.clientWidth, 360) + 12)}
       y={60}
       z={100}
+      width={Math.min(document.body.clientWidth, 360)}
+      height={document.body.clientHeight - 120}
       url="help?header=false"
       themeColor="#7aa1d6"
+      noMax={true}
       onClose={() => setIsShowHelpPage(false)}
     />
   );
