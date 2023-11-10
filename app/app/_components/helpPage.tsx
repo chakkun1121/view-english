5027270f55a393fc3d685cab96f264bc9897811d
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2023-11-10 03:55:04 +0000

    高さを調節

--- app/app/_components/helpPage.tsx
+++ app/app/_components/helpPage.tsx
@@ -10,13 +10,13 @@ export function HelpPage({
     <WinBox
       title="ヘルプ"
       noFull={true}
-      top={60}
-      bottom={60}
+      top={document.body.clientWidth < 768 ? 0 : 60}
+      bottom={document.body.clientWidth < 768 ? 60 : 0}
       x={document.body.clientWidth - (Math.min(document.body.clientWidth, 360) + 12)}
       y={60}
       z={100}
       width={Math.min(document.body.clientWidth, 360)}
-      height={document.body.clientHeight - 120}
+      height={document.body.clientHeight - 70}
       url="embeddedHelpPage"
       themeColor="#7aa1d6"
       noMax={true}
--- app/app/_components/settingsPage.tsx
+++ app/app/_components/settingsPage.tsx
@@ -16,13 +16,13 @@ export default function SettingsPage({
       onClose={() => setIsShowSettings(false)}
       noMax={true}
       noFull={true}
-      top={60}
-      bottom={60}
+      top={document.body.clientWidth < 768 ? 0 : 60}
+      bottom={document.body.clientWidth < 768 ? 60 : 0}
       x={document.body.clientWidth - (Math.min(document.body.clientWidth, 360) + 20)}
       y={60}
       z={100}
       width={Math.min(document.body.clientWidth, 360)}
-      height={document.body.clientHeight - 120}
+      height={document.body.clientHeight - 70}
       className="flex"
     >
       <div className="flex-1 flex flex-col gap-4 p-4 bg-primary h-full">
